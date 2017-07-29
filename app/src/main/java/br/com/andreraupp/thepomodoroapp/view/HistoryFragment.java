25ec2fd5c95717d4f3211de2d7a351a07889df03
andreraupp <andreraupp@gmail.com>
2017-07-29 10:21:58 -0300

    Implemented the display of the pomodoro history list

--- app/src/main/java/br/com/andreraupp/thepomodoroapp/view/HistoryFragment.java
+++ app/src/main/java/br/com/andreraupp/thepomodoroapp/view/HistoryFragment.java
@@ -5,19 +5,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.andreraupp.thepomodoroapp.R;
+import br.com.andreraupp.thepomodoroapp.ThePomodoro;
+import br.com.andreraupp.thepomodoroapp.controller.PomodoroController;
+import br.com.andreraupp.thepomodoroapp.model.Pomodoro;
 
 /**
  * Created by andre on 26/07/2017.
  */
 
 public class HistoryFragment extends Fragment {
+    private ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_history, container, false);
-
+        listView = (ListView) rootView.findViewById(R.id.list_pomodoro);
+        startAdapter();
         return rootView;
+    }
+
+    public void startAdapter() {
+        PomodoroAdapter pomodoroListAdapter;
+        PomodoroController pomodoroController = new PomodoroController();
+        List<Pomodoro> pomodoroList = pomodoroController.getPomodoroList();
+
+        pomodoroListAdapter = new PomodoroAdapter(pomodoroList, ThePomodoro.getContext());
+        listView.setAdapter(pomodoroListAdapter);
     }
 }

b7403959228a86ee19740b2f8a44419406c5db42
andreraupp <andreraupp@gmail.com>
2017-07-29 10:25:27 -0300

    Implemented refresh in HistoryFragment

--- app/src/main/java/br/com/andreraupp/thepomodoroapp/view/PomodoroActivity.java
+++ app/src/main/java/br/com/andreraupp/thepomodoroapp/view/PomodoroActivity.java
@@ -4,34 +4,51 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import br.com.andreraupp.thepomodoroapp.R;
 
 public class PomodoroActivity extends AppCompatActivity {
+    private HistoryFragment historyFragment;
 
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pomodoro);
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        mViewPager = (ViewPager) findViewById(R.id.container);
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 1) {
+                    historyFragment.startAdapter();
+                }
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+
     }
 
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private class SectionsPagerAdapter extends FragmentStatePagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        private SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
@@ -41,7 +58,7 @@ public class PomodoroActivity extends AppCompatActivity {
                 case 0:
                     return new NewFragment();
                 case 1:
-                    return new HistoryFragment();
+                    return  historyFragment = new HistoryFragment();
             }
             return null;
         }
